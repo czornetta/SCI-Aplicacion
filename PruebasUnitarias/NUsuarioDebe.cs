f2a387559db04c43b8b709727c7ffab2150649cd
czornetta <czornetta@hotmail.com>
2020-11-14 15:30:46 -0300

    Se agregaron las pruebas de inicio de sesion.

--- PruebasUnitarias/NUsuarioDebe.cs
+++ PruebasUnitarias/NUsuarioDebe.cs
@@ -12,19 +12,57 @@ namespace PruebasUnitarias
     public class NUsuarioDebe
     {
         [Theory]
-        [InlineData("adm", "123", LoginResult.ValidUser)]
+        [InlineData("admi", "123",LoginResult.InvalidUsername)]
+        [InlineData("admin", "1", LoginResult.InvalidPassword)]
+        [InlineData("admin", "123", LoginResult.ValidUser)]
         public void IniciarSesion(string nombre, string clave, LoginResult rEsperado)
         {
             // Arrange
             NUsuario nUsuario = new NUsuario();
+            LoginResult resultado;
 
             // Act
-            LoginResult resultado = nUsuario.IniciarSesion(nombre, clave);
+            try
+            {
+                resultado = nUsuario.IniciarSesion(nombre, clave);
+                nUsuario.CerrarSesion();
+            }
+            catch (LoginException ex)
+            {
 
+                resultado = ex.Result;
+            }
+            
             // Assert
             Assert.Equal(rEsperado, resultado);
 
         }
 
+        [Theory]
+        [InlineData("admin", "123", LoginResult.ExistsActiveSesion)]
+        public void InformarExisteSesionActiva(string nombre, string clave, LoginResult rEsperado)
+        {
+            // Arrange
+            NUsuario nUsuario = new NUsuario();
+            LoginResult resultado;
+            string usr = "jose";
+            string psw = "123";
+
+            // Act
+            try
+            {
+                resultado = nUsuario.IniciarSesion(usr, psw);
+                resultado = nUsuario.IniciarSesion(nombre, clave);
+            }
+            catch (LoginException ex)
+            {
+
+                resultado = ex.Result;
+            }
+
+            // Assert
+            Assert.Equal(rEsperado, resultado);
+
+        }
     }
 }
